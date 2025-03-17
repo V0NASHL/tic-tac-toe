7b91ce8538e49c2157155e8d93f69750a43a5c01
V0NASHL <136652079+V0NASHL@users.noreply.github.com>
2025-03-17 13:52:14 -0400

    add function to end game

--- script.js
+++ script.js
@@ -12,25 +12,25 @@ const cellElements = document.querySelectorAll(".cell");
 function Player(name, marker) {
     this.name = name;
     this.marker = marker;
-};
+}
 
 cellElements.forEach((cell, index) => {
     cell.addEventListener("click", () => {
         placeMarker(index);
     });
-});
+})
 
 function placeMarker(index) {
     let col = index % 3
     let row = (index - col) / 3
-    if(boardData[row][col] == 0) {
+    if(boardData[row][col] == 0 && gameOver == false) {
         boardData[row][col] = mark;
         mark *= -1;
         drawMarkers();
     }
 
     checkResult();
-};
+}
 
 function drawMarkers() {
     for(let row = 0; row < 3; row++) {
@@ -50,10 +50,12 @@ function checkResult() {
         let rowSum = boardData[i][0] + boardData[i][1] + boardData[i][2];
         let colSum = boardData[0][i] + boardData[1][i] + boardData[2][i];
         if(rowSum == 3 || colSum == 3) {
-            console.log("Player 1 Wins!")
+            endGame(1);
+            return;
         }
         else if(rowSum == -3 || colSum == -3) {
-            console.log("Player 2 Wins!")
+            endGame(2);
+            return;
         }
     }
 
@@ -61,13 +63,27 @@ function checkResult() {
     let diagonalSum2 = boardData[0][2] + boardData[1][1] + boardData[2][0];
     
     if(diagonalSum1 == 3 || diagonalSum2 == 3) {
-        console.log("Player 1 Wins!")
+        endGame(1);
+        return;
     }
     else if(diagonalSum1 == -3 || diagonalSum2 == -3) {
-        console.log("Player 2 Wins!")
+        endGame(2);
+        return;
     }
 
     if(boardData[0].indexOf(0) == -1 && boardData[1].indexOf(0) == -1 && boardData[2].indexOf(0) == -1) {
-        console.log("Tie");
+        endGame(0);
+        return;
+    }
+}
+
+function endGame(winner) {
+    gameOver = true;
+
+    if(winner == 0) {
+        console.log("Tie")
+    }
+    else {
+        console.log(`Player ${winner} wins!`);
     }
 }
